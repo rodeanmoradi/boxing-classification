d05e21a36f5f82d7ba03d9f833c1967e91cf06fd
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2025-11-15 21:29:42 -0500

    basic script

--- src/camera_script.py
+++ src/camera_script.py
@@ -1,4 +1,14 @@
 import cv2
 
-webcam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
 
+while True:
+    ret, frame = cam.read()
+    
+    cv2.imshow('MacBook Camera', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.release()
+cv2.destroyAllWindows()
