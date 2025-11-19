4a4671a80e89b2b70273feb54a4af193971f545c
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2025-11-19 02:18:08 -0500

    Created preprocess function to turn webcame frame tensor into tensor input format for model

--- src/camera_script.py
+++ src/camera_script.py
@@ -1,10 +1,28 @@
 import cv2
+import tensorflow as tf
+import numpy as np
+
+# Turn original image tensor into input format
+def preprocess(frame):
+    img = cv2.resize(frame, (256, 256))
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+
+    return img
 
 cam = cv2.VideoCapture(0)
 
+# Load MoveNet Thunder
+path = 'models/movenet/movenet_thunder.tflite'
+interpreter = tf.lite.Interpreter(model_path=path)
+interpreter.allocate_tensors()
+
+# Display webcame
 while True:
     ret, frame = cam.read()
     
+    preprocess(frame)
+
     cv2.imshow('MacBook Camera', frame)
 
     if cv2.waitKey(1) == ord('x'):
--- src/movenet.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-path = 'models/movenet/movenet_thunder.tflite'
-interpreter = tf.lite.Interpreter(model_path=path)
-interpreter.allocate_tensors()
