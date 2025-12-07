209aad3a2d56fa3cc0bbc73adfeeaeea9a0aa4bf
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2025-12-07 03:52:53 -0500

    Moved connections out of loop

--- src/script.py
+++ src/script.py
@@ -38,6 +38,24 @@ cam = cv2.VideoCapture(1)
 
 inputDetails, outputDetails = deployMovenet(interpreter)
 
+connections = [
+        (0, 1),
+        (0, 2),
+        (1, 3),
+        (2, 4),
+        (5, 6),
+        (5, 7),
+        (5, 11),
+        (6, 8),
+        (6, 12),
+        (7, 9),
+        (8, 10),
+        (11, 13),
+        (12, 14),
+        (13, 15),
+        (14, 16)
+    ]
+
 ringBuffer = np.zeros((N, 17, 2))
 while True:
     ret, frame = cam.read()
@@ -67,24 +85,6 @@ while True:
             ringBuffer[curFrame, i, 1] = ringBuffer[lastFrame, i, 1]
     smoothed = np.mean(ringBuffer, axis=0)
 
-    connections = [
-        (0, 1),
-        (0, 2),
-        (1, 3),
-        (2, 4),
-        (5, 6),
-        (5, 7),
-        (5, 11),
-        (6, 8),
-        (6, 12),
-        (7, 9),
-        (8, 10),
-        (11, 13),
-        (12, 14),
-        (13, 15),
-        (14, 16)
-    ]
-    
     # Draw keypoints and connections
     for (start, end) in connections:
         cv2.line(frame, (int(smoothed[start][0]), int(smoothed[start][1])), (int(smoothed[end][0]), int(smoothed[end][1])), (255, 0, 0), 3)
