a677021641a396f477269dcd03ff0d3d48b0fcf1
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2025-12-22 20:47:48 -0500

    Got rid of for loops for one euro filter, now using numpy for matrix operations (all at once instead of point by point)

--- src/script.py
+++ src/script.py
@@ -46,9 +46,9 @@ class SmoothingFilter:
         self.b = 5.0
         self.f_c_min = 1.0
         self.f_c_d = 0.5
-        self.prev_point = np.zeros((1, 17, 2))
-        self.prev_speed = np.zeros((1, 17, 2))
-        self.smoothed = np.zeros((1, 17, 2))
+        self.prev_points = np.zeros((17, 2))
+        self.prev_speed = np.zeros((17, 2))
+        self.smoothed = np.zeros((17, 2))
 
     # TODO: Tune and use NumPy vectorization to improve latency
     def filter(self, output, frame_height, frame_width, sampling_period):
@@ -56,68 +56,37 @@ class SmoothingFilter:
         if sampling_period <= 0.00001: 
             sampling_period = 0.016
         
-        # NumPy vectorization
-        # Slice output -> big array of all output points
-        points = output[0, 0, :, :].copy() # size is 17x3
-        # Check confidence scores
-        # Smooth speed all at once
-        # Smooth position all at once
-        # Output smoothed
+        coords = output[0, 0, :, :2].copy()
+        coords = coords[:, [1, 0]]
+        coords = coords * np.array([frame_width, frame_height])
 
-        for i in range(17):
-            point = output[0, 0, i, :]
-            y_point = point[0] * frame_height
-            x_point = point[1] * frame_width
-            confidence = point[2]
+        if np.all(self.prev_points == 0):
+            self.prev_points = coords.copy()
+            self.smoothed = coords.copy()
+
+        confidence = output[0, 0, :, 2].copy()
+        mask = confidence > self.confidence_threshold
+        mask = mask[:, np.newaxis]
+
+        x_dot = (coords - self.prev_points) / sampling_period
+        a_d = 1 / (1 + (1 / (2 * PI * self.f_c_d * sampling_period)))
+        x_dot_hat = x_dot * a_d + self.prev_speed * (1 - a_d)
+
+        f_c = self.f_c_min + self.b * abs(x_dot_hat)
+        a = 1 / (1 + (1 / (2 * PI * f_c * sampling_period)))
+        x_hat = coords * a + self.smoothed * (1 - a)
+
+        self.smoothed = np.where(mask, x_hat, self.smoothed)
+        self.prev_speed = np.where(mask, x_dot_hat, self.prev_speed)
+        self.prev_points = np.where(mask, coords, self.prev_points)
         
-            if confidence > self.confidence_threshold:
-
-                # Smooth speed
-                x_dot_0 = (x_point - self.prev_point[0, i, 0]) / sampling_period
-                x_dot_1 = (y_point - self.prev_point[0, i, 1]) / sampling_period
-                a_d = 1 / (1 + (1 / (2 * PI * self.f_c_d * sampling_period)))
-                x_dot_hat_0 = x_dot_0 * a_d + self.prev_speed[0, i, 0] * (1 - a_d)
-                x_dot_hat_1 = x_dot_1 * a_d + self.prev_speed[0, i, 1] * (1 - a_d)
-
-                # Smooth position 
-                f_c_0 = self.f_c_min + self.b * abs(x_dot_hat_0)
-                a_0 = 1 / (1 + (1 / (2 * PI * f_c_0 * sampling_period)))
-                x_hat_0 = x_point * a_0 + self.smoothed[0, i, 0] * (1 - a_0)
-                f_c_1 = self.f_c_min + self.b * abs(x_dot_hat_1)
-                a_1 = 1 / (1 + (1 / (2 * PI * f_c_1 * sampling_period)))
-                x_hat_1 = y_point * a_1 + self.smoothed[0, i, 1] * (1 - a_1)
-
-                self.smoothed[0, i, 0] = x_hat_0
-                self.smoothed[0, i, 1] = x_hat_1
-
-                self.prev_speed[0, i, 0] = x_dot_hat_0
-                self.prev_speed[0, i, 1] = x_dot_hat_1
-                
-                self.prev_point[0, i, 0] = x_point
-                self.prev_point[0, i, 1] = y_point 
-
-        return self.smoothed
+        return self.smoothed[np.newaxis, :, :]
 
 class Visualiser:
     
     def __init__(self):
-        self.connections = [
-            (0, 1),
-            (0, 2),
-            (1, 3),
-            (2, 4),
-            (5, 6),
-            (5, 7),
-            (5, 11),
-            (6, 8),
-            (6, 12),
-            (7, 9),
-            (8, 10),
-            (11, 13),
-            (12, 14),
-            (13, 15),
-            (14, 16)
-        ]
+        self.connections = [(0, 1), (0, 2), (1, 3), (2, 4), (5, 6), (5, 7), (5, 11), (6, 8), (6, 12), (7, 9),
+            (8, 10), (11, 13), (12, 14), (13, 15), (14, 16)]
         
     def draw_keypoints(self, frame, smoothed):
         for (start, end) in self.connections:
