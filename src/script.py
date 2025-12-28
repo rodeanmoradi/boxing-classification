6dac723ed2358ae0be94d7d5dfd29b0b7f8cecbf
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2025-12-28 16:17:00 +0400

    Created LSTM model architecture

--- src/script.py
+++ src/script.py
@@ -129,9 +129,16 @@ class Buffer:
 
 class LSTM(nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super().__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, num_classes)
+    
+    def forward(self, input):
+        _, (h_n, _) = self.lstm(input)
+        classification = self.linear(h_n[-1])
 
+        return classification
 
 movenet_lightning = PoseEstimator('models/movenet/movenet_lightning.tflite')
 one_euro = SmoothingFilter()
