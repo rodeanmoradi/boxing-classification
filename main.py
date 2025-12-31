5cd18666711d79f8c2927e1511295f6b007bb5c7
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2025-12-31 12:34:44 +0400

    Created main function

--- main.py
+++ main.py
@@ -3,37 +3,42 @@ import time
 
 from src import PoseEstimator, SmoothingFilter, Visualiser, Buffer
 
-cam = cv2.VideoCapture(1) # Pass 0 or 1
-frame_height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-frame_width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
-prev_time = time.time()
+def main():
 
-movenet_lightning = PoseEstimator('models/movenet/movenet_lightning.tflite')
-one_euro = SmoothingFilter()
-visualiser = Visualiser()
-circular_buffer = Buffer()
+    cam = cv2.VideoCapture(1) # Pass 0 or 1
+    frame_height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    frame_width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+    prev_time = time.time()
 
-while True:
-    current_time = time.time()
-    frame_time = current_time - prev_time
-    prev_time = current_time
+    movenet_lightning = PoseEstimator('models/movenet/movenet_lightning.tflite')
+    one_euro = SmoothingFilter()
+    visualiser = Visualiser()
+    circular_buffer = Buffer()
     
-    ret, frame = cam.read()
-    
-    if not ret:
-        print("Failed to read")
-        break
+    while True:
+        current_time = time.time()
+        frame_time = current_time - prev_time
+        prev_time = current_time
+        
+        ret, frame = cam.read()
+        
+        if not ret:
+            print("Failed to read")
+            break
 
-    smoothed = one_euro.filter(movenet_lightning.detect(frame), frame_height, frame_width, frame_time)
+        smoothed = one_euro.filter(movenet_lightning.detect(frame), frame_height, frame_width, frame_time)
 
-    visualiser.draw_keypoints(frame, smoothed)
+        visualiser.draw_keypoints(frame, smoothed)
 
-    circular_buffer.fill_buffer(smoothed)
+        circular_buffer.fill_buffer(smoothed)
 
-    cv2.imshow('MacBook Camera', frame)
+        cv2.imshow('MacBook Camera', frame)
 
-    if cv2.waitKey(1) == ord('x'):
-        break
+        if cv2.waitKey(1) == ord('x'):
+            break
 
-cam.release()
-cv2.destroyAllWindows()
+    cam.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
