a59f8a47ec7c872baf79a717e888c139318395d1
Rodean Moradi <rodeanmoradi@users.noreply.github.com>
2026-01-20 14:56:47 -0500

    Updated src init file

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@ from .pose_estimation import PoseEstimator
 from .smoothing_filter import SmoothingFilter
 from .visualiser import Visualiser
 from .circular_buffer import Buffer
+from .lstm_model import LSTM
